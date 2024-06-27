b8fa22c6bc264b704336f09e511dcd095aa500e7
Lorenzo <lorenzobadilla10@gmail.com>
2024-06-27 23:16:00 +0000

    🚀

--- public/downloader/dl.js
+++ public/downloader/dl.js
@@ -13,7 +13,7 @@ const response = await aio(link)
 console.log(response) 
 const title = response.result.title;
 const video = response.result.medias[1].url;
-res.json({url: video, title: title, creator: "Eugene Aguilar"})
+res.json({url: video, title: title, creator: "Lorenzo"})
 } catch(error) {
 res.json({error: "error fetching downloader api"})
 console.log(error)
