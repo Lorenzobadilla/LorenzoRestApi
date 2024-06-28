b6f4c3587fa43a06572402f8292d6a3c6b6421d7
Lorenzo <lorenzobadilla10@gmail.com>
2024-06-28 10:14:10 +0000

    Fixed some bug pickupline

--- public/pickupline/pickupline.js
+++ public/pickupline/pickupline.js
@@ -4,11 +4,10 @@ const fs = require("fs");
 exports.name = "/api/pickupline";
 exports.index = async function (req, res) {
     try {
-        const data = fs.readFileSync(path.join(__dirname, "data", "pickupline.json"), "utf-8");
-        const pickupline = JSON.parse(data); 
-        var random = Math.floor(Math.random() * couple.length);
+        const pickupline = JSON.parse(fs.readFileSync('pickupline.json'));
+        const data = pickupline[Math.floor(Math.random() * pickupline.length)];
         return res.json({
-        pickupline: pickupline[random]
+            pickupline: data
         });
     } catch (error) {
         res.status(500).json({ error: error.message })
