3ca246835638f55891cea8ce4ace7360c0706fe6
Lorenzo <lorenzobadilla10@gmail.com>
2024-07-14 03:54:25 +0000

    Testing

--- public/joke/joke.js
+++ public/joke/joke.js
@@ -4,10 +4,17 @@ const fs = require("fs");
 exports.name = "/api/joke";
 exports.index = async function (req, res) {
     try {
-        const joke = JSON.parse(fs.readFileSync('data/joke.json'));
-        const data = joke[Math.floor(Math.random() * joke.length)];
+        const response = await axios.get("https://v2.jokeapi.dev/joke/Any");
+        let jokeData;
+        if (Array.isArray(response.data)) {
+            // If it's an array, pick a random joke
+            jokeData = response.data[Math.floor(Math.random() * response.data.length)];
+        } else {
+            // If it's a single object, use that directly
+            jokeData = response.data;
+        }
         return res.json({
-            joke: data
+            joke: jokeData
         });
     } catch (error) {
         res.status(500).json({ error: error.message })
--- public/pickupline/pickupline.js
+++ public/pickupline/pickupline.js
@@ -1,10 +1,14 @@
-const path = require("path");
+const axios = require('axios');
 const fs = require("fs");
 
 exports.name = "/api/pickupline";
 exports.index = async function (req, res) {
     try {
-        const pickupline = JSON.parse(fs.readFileSync('data/pickupline.json'));
+        const response = await fetch('https://rizzapi.vercel.app/random');
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const pickupline = await response.json();
         const data = pickupline[Math.floor(Math.random() * pickupline.length)];
         return res.json({
             pickupline: data
